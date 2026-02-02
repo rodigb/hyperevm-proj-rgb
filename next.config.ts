960ac00446537706a28fbf2bbd25baf7e1a89dcc
rodigb <rodix08@gmail.com>
2026-02-02 23:53:30 +0000

    deployment change

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-   images: {
+  output: "export",
+  images: {
     remotePatterns: [
       {
         protocol: "https",
