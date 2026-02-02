cda61a78a24987759ddb3a46cf9312c08d1c40b2
rodigb <rodix08@gmail.com>
2026-02-02 20:16:41 +0000

    edited config for deployment

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  output: "export",
   images: {
     remotePatterns: [
       {
@@ -13,3 +14,4 @@ const nextConfig: NextConfig = {
 };
 
 export default nextConfig;
+ 
